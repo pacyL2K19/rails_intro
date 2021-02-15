4c936701ef441d42a3e9f94fd5316a29db452aa3
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-02-15 13:44:43 +0100

    Minor update

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
+  get "about", to:"about#index"
+
   root 'cars#index'
   resources :cars
-end
-
-Rails.application.routes.draw do
-  get "about", to:"about#index"
 end
